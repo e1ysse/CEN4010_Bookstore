d8760ffa2e8e436f182879bcfc87fe0620adbaad
Elysse Viena <elysseviena23@gmail.com>
2023-09-21 21:27:14 -0400

    changed some things in controller,,,, idk

--- src/main/java/com/jetbrains/geektext/GeekText/Controller.java
+++ src/main/java/com/jetbrains/geektext/GeekText/Controller.java
@@ -1,9 +1,9 @@
 package com.jetbrains.geektext.GeekText;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "TEAM 30 !!!!!!";
     }
--- src/main/java/com/jetbrains/geektext/GeekText/SpringbootPackageApplication.java
+++ src/main/java/com/jetbrains/geektext/GeekText/SpringbootPackageApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootPackageApplication {
-
+//open http://localhost:8080/ on any web browser and run
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootPackageApplication.class, args);
 	}
