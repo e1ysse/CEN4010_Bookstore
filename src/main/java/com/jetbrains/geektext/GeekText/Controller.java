b8c45a11abf7e24bee8aa9d293a81d4342bcf14c
Elysse Viena <elysseviena23@gmail.com>
2023-09-28 20:58:02 -0400

    this one worksgit add . kinda

---
+++ src/main/java/com/jetbrains/geektext/GeekText/Controller.java
@@ -0,0 +1,11 @@
+package com.jetbrains.geektext.GeekText;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public String index() {
+        return "TEAM 30 !!!!!!";
+    }
+
+}
